6100f82cecc6318ba9b52dce92f0bb5c504cdb94
Clapps-lmao <90117687+Clapps-lmao@users.noreply.github.com>
2023-04-14 09:58:54 -0700

    Update user-agent-guard.js

--- user-agent-guard.js
+++ user-agent-guard.js
@@ -1,30 +1,36 @@
-// Check the initial user agent
-var userAgent = navigator.userAgent;
+document.addEventListener("DOMContentLoaded", function() {
+  // Check the initial user agent and touch support
+  var userAgent = navigator.userAgent;
+  var touchSupport = navigator.maxTouchPoints > 0;
 
-// Listen for user agent changes
-window.addEventListener("useragentchanged", function() {
-  // Get the new user agent
-  var newUserAgent = navigator.userAgent;
+  // Listen for user agent or touch support changes
+  window.addEventListener("useragentchanged", function() {
+    // Get the new user agent and touch support
+    var newUserAgent = navigator.userAgent;
+    var newTouchSupport = navigator.maxTouchPoints > 0;
 
-  // Check if the user agent has changed
-  if (userAgent !== newUserAgent) {
-    // Redirect to the unsupported page
-    window.location.href = "https://clapps.lol/unsupported";
-  }
+    // Check if the user agent or touch support has changed
+    if (userAgent !== newUserAgent || touchSupport !== newTouchSupport) {
+      // Redirect to the unsupported page
+      window.location.href = "https://Clapps.lol/unsupported";
+    }
+  });
+
+  // Function to check for user agent or touch support changes every 1 second
+  setInterval(function() {
+    // Get the current user agent and touch support
+    var currentUserAgent = navigator.userAgent;
+    var currentTouchSupport = navigator.maxTouchPoints > 0;
+
+    // Check if the user agent or touch support has changed
+    if (userAgent !== currentUserAgent || touchSupport !== currentTouchSupport) {
+      // Dispatch the useragentchanged event
+      var event = new Event("useragentchanged");
+      window.dispatchEvent(event);
+
+      // Update the user agent and touch support variables
+      userAgent = currentUserAgent;
+      touchSupport = currentTouchSupport;
+    }
+  }, 50);
 });
-
-// Function to check for user agent changes every 1 second
-setInterval(function() {
-  // Get the current user agent
-  var currentUserAgent = navigator.userAgent;
-
-  // Check if the user agent has changed
-  if (userAgent !== currentUserAgent) {
-    // Dispatch the useragentchanged event
-    var event = new Event("useragentchanged");
-    window.dispatchEvent(event);
-
-    // Update the user agent variable
-    userAgent = currentUserAgent;
-  }
-}, 500);
